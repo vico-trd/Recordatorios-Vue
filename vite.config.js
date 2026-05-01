b49adbfac8577669c9f481c82a751c38810c5806
Alex <vicotrdinfrx@gmail.com>
2026-05-01 22:09:27 +0200

    Revertir cambios de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Recordatorios-Vue/',
   plugins: [vue()],
   optimizeDeps: {
     include: ['firebase/app', 'firebase/auth'],
